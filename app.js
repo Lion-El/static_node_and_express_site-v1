2b5317e4104e5ee5f4fbd2b12bcfa659015c15b6
Lion-El <68742917+Lion-El@users.noreply.github.com>
2024-02-10 06:40:04 +0000

    error handlers added

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
 const express = require('express');
 const path = require('path');
+
+// import route definitions
 const routes = require('./routes');
 
+// Instantiate Express app
 const app = express();
 
+// Setup view engine
 app.use('/static', express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'views')));
 app.set('view engine', 'pug');
 
+// Express middleware/route definitions
 app.use(routes);
+
+// ERROR HANDLERS
+// 404 handler to catch undefined or non-existent route requests 
+app.use((res, req, next) => {
+    const err = new Error('Hey! That webpage does not exist');
+    err.status = 404;
+    console.log(`${err.message} (error:${err.status})`);
+});
+
+// Global error handler
+app.use((err, res, req, next) => {
+    if (err === 404) {
+        console.log(`${err.message} (error:${err.status})`);
+    }
+    
+});
 
 app.listen(3000);
--- routes/index.js
+++ routes/index.js
@@ -3,16 +3,30 @@ const router = express.Router();
 const { data } = require('../data.json');
 const { projects } = data;
 
+// Render index page
 router.get('/', (req, res) => {
     res.locals.projects = data.projects;
-    console.log(res.locals.projects);
     res.render('index');
 });
 
+// Render about page
 router.get('/about', (req, res) => {
-    res.locals = data.projects;
-    console.log(res.locals);
     res.render('about');
+});
+
+// render individual project
+router.get('/project/:id', (req, res) => {
+    res.locals.project = data.projects;
+    const id = req.params.id;
+
+    if (projects[id]) {
+        res.render('project', { id });
+    } else {
+        const err = new Error('Hey! That webpage does not exist');
+        err.status = 404;
+        console.log(`${err.message} (error:${err.status})`);
+        next(err);
+    }
 });
 
 module.exports = router;
